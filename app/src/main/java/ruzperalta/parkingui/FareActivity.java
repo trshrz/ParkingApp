cfbfabc6ef0c83bc56dc5235658ff77e78bfdf9d
Raffy Peralta <raffy.peralta.01@gmail.com>
2018-12-14 20:45:42 +0800

    rfy

--- app/src/main/java/ruzperalta/parkingui/FareActivity.java
+++ app/src/main/java/ruzperalta/parkingui/FareActivity.java
@@ -7,12 +7,14 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -28,7 +30,8 @@ public class FareActivity extends Activity {
     int currentHour;
     int currentMinute;
     String amPm;
-
+    int hr, endhr;
+    Button btnCalculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +39,31 @@ public class FareActivity extends Activity {
         startTime = (TextView) findViewById(R.id.startTime);
         endTime = (TextView) findViewById(R.id.endTime);
         btnParking = findViewById(R.id.btnParking);
+        btnCalculate = findViewById(R.id.btnCalculate);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int fare,calculate;
+                Log.e("Raffy", hr + "");
+                Log.e("Raffy", endhr + "");
+                if(hr > endhr){
+                    endhr = endhr + 24;
+                }
+                calculate = endhr - hr;
+
+                if(calculate > 3){
+                    calculate = calculate - 3;
+                    fare = 50 + (calculate*10);
+                }else if(calculate == 0){
+                    fare = 150;
+                }else{
+                    fare = 50;
+                }
+
+                Toast.makeText(FareActivity.this, "Fare is " + fare + " pesos.", Toast.LENGTH_LONG).show();
+            }
+        });
         startTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,11 +77,13 @@ public class FareActivity extends Activity {
                         if (hourOfDay >= 12) {
                             amPm = "PM";
                             if(hourOfDay > 12){
-                                hourOfDay = hourOfDay - 12;
+
+//                                hourOfDay = hourOfDay - 12;
                             }
                         } else {
                             amPm = "AM";
                         }
+                        hr = hourOfDay;
                         startTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
                     }
                 }, currentHour, currentMinute, false);
@@ -78,12 +108,13 @@ public class FareActivity extends Activity {
                         if (hourOfDay >= 12) {
                             amPm = "PM";
                             if(hourOfDay > 12){
-                                hourOfDay = hourOfDay - 12;
+
+//                                hourOfDay = hourOfDay - 12;
                             }
                         } else {
                             amPm = "AM";
                         }
-
+                        endhr = hourOfDay;
                         endTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
                     }
                 }, currentHour, currentMinute, false);
